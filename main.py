e135503df8f6b6513e0e4a0740f4064e65f4a48c
chanlim2004 <83573839+chanlim2004@users.noreply.github.com>
2025-10-28 13:17:01 +0900

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import
-    random_int = random.randint(1, 10)
-    print(f"Random integer: {random_int}")
-
-    random_float = random.random()
-    print(f"Random float: {random_float}")
-
-    random_uniform = random.uniform(5.0, 15.0)
-    print(f"Random uniform float: {random_uniform}")
